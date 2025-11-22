58056eb437bdcf8ab2a87c8e9248b11d6a5a87d5
Diego-Eduardo-da-Silva-Santos <diegodidi2005@gmail.com>
2025-11-22 00:46:37 -0300

    test: add Function for getting Token

--- resource-sever/src/main/java/br/lamentina/resource_sever/ResourceSeverApplication.java
+++ resource-sever/src/main/java/br/lamentina/resource_sever/ResourceSeverApplication.java
@@ -2,6 +2,11 @@ package br.lamentina.resource_sever;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.oauth2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ResourceSeverApplication {
@@ -10,4 +15,15 @@ public class ResourceSeverApplication {
 		SpringApplication.run(ResourceSeverApplication.class, args);
 	}
 
+
+
+}
+
+@RestController
+@RequestMapping("hello")
+class HelloController {
+	@GetMapping
+	public String hello(@AuthenticationPrincipal Jwt jwt){
+		return "OlaMundo\n" + jwt.getTokenValue();
+	}
 }
