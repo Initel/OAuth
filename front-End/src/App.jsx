c0e65985fe48690b13f9ee09d39f6dfceefff862
Initel <diegoedusantos1205@gmail.com>
2025-11-22 20:35:20 -0300

    Feat: Projeto concluido

--- front-End/src/App.jsx
+++ front-End/src/App.jsx
@@ -1,25 +1,18 @@
 function App() {
-  return 
+  return <main className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <div className="container text-center">
+        <div className="d-grid gap-2 col-4 mx-auto">
+          <a
+            className="w-100 btn btn-lg btn-primary"
+            href="http://localhost:3000/oauth2/authorization/keycloak"
+            role="button"
+            style={{ marginTop: "10px" }}
+          >
+            Login
+          </a>
+        </div>
+      </div>
+    </main>
 }
 
 export default App
-
-<main>
-    <head>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-    </head>
-    <body>
-      <div class="container">
-        <br />
-        <div class="d-grid gap-2 col-4 mx-auto">
-          <br />
-          <form class="form-signin w-100 m-auto">
-            <a class="w-100 btn btn-lg btn-primary" href="http://localhost:3000/oauth2/authorization/keycloak"
-              role="link" style={{ marginTop: '10px' }}>
-              Login
-            </a>
-          </form>
-        </div>
-      </div>
-    </body>
-  </main>
