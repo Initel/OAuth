47ea0cd1a263cff06acd1d2cd2e1fa730cfcfd80
Initel <diegoedusantos1205@gmail.com>
2025-11-22 15:01:48 -0300

    build: finalizado o gateway

--- gateway/src/main/java/com/br/lamentavel/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/br/lamentavel/gateway/GatewayApplication.java
@@ -1,7 +1,16 @@
 package com.br.lamentavel.gateway;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +19,20 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+}
+
+@RestController
+class ProfileController {
+	@GetMapping("userinfo")
+	public Map<String, Object> userinfo(@AuthenticationPrincipal OidcUser oidcUser,
+	@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient){
+		var attributesMap = new HashMap<>(oidcUser.getAttributes());
+		attributesMap.put("token", oidcUser.getIdToken().getTokenValue());
+		attributesMap.put("clientName", authorizedClient.getClientRegistration().getClientId());
+		attributesMap.put("userAttributes", oidcUser.getAttributes());
+		return attributesMap;
+		 
+	}
+
+	
 }
