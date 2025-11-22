7a658eb7085d777c8a7aed7b653d7c6d238a6bbd
Diego-Eduardo-da-Silva-Santos <diegodidi2005@gmail.com>
2025-11-22 11:13:47 -0300

    feat: reiniciando o projeto

--- sevured-service/src/main/java/br/com/lamentavel/sevured_service/SevuredServiceApplication.java
+++ sevured-service/src/main/java/br/com/lamentavel/sevured_service/SevuredServiceApplication.java
@@ -1,13 +1,13 @@
-package br.com.lamentavel.cliente_server;
+package br.com.lamentavel.sevured_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClienteServerApplication {
+public class SevuredServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClienteServerApplication.class, args);
+		SpringApplication.run(SevuredServiceApplication.class, args);
 	}
 
 }
--- sevured-service/src/test/java/br/com/lamentavel/sevured_service/SevuredServiceApplicationTests.java
+++ sevured-service/src/test/java/br/com/lamentavel/sevured_service/SevuredServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package br.lamentina.resource_sever;
+package br.com.lamentavel.sevured_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ResourceSeverApplicationTests {
+class SevuredServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
